8e3259f86cb66dd319993fd2a0e14fd17aaa8470
berniejenny <berniejenny@users.noreply.github.com>
2024-03-23 17:04:53 +1100

    Show progress view while loading texture; indicate selected globe in list.

--- Globes/Views/GlobeSelectionView.swift
+++ Globes/Views/GlobeSelectionView.swift
@@ -10,9 +10,15 @@ import SwiftUI
 
 /// A view with name, author and 3D model for a globe
 struct GlobeSelectionView: View {
+    
+    /// The view model contains the currently selected globe.
     @Environment(ViewModel.self) private var model
     
+    /// The globe that can be selected by pinching this view
     @State private var configuration: GlobeEntity.Configuration
+    
+    /// Flag to show progress view while loading large globe texture
+    @State private var loadingTexture = false
     
     private static let globeViewSize: CGFloat = 100
     private static let height = 1.1 * globeViewSize
@@ -32,16 +38,14 @@ struct GlobeSelectionView: View {
             addHoverEffect: false // hover effect on the globe is potentially confusing, because the background changes color when the globe is hovered.
         )
     }
-    
-    private var globe: Globe { configuration.globe }
-    
+        
     var body: some View {
-        let authorAndDate = globe.authorAndDate
+        let authorAndDate = configuration.globe.authorAndDate
         
         ZStack(alignment: .leading) {
             // name and author
             VStack(alignment: .leading) {
-                Text(globe.name)
+                Text(configuration.globe.name)
                     .font(.headline)
                 Text(authorAndDate)
                     .font(.callout)
@@ -49,6 +53,16 @@ struct GlobeSelectionView: View {
                     .opacity(authorAndDate.isEmpty ? 0 : 1)
             }
             .padding(.leading)
+            
+            HStack {
+                Spacer()
+                ProgressView()
+                    .padding()
+                    .glassBackgroundEffect()
+                Spacer()
+            }
+            .offset(z: 20)
+            .opacity(loadingTexture ? 1 : 0)
             
             HStack {
                 Spacer()
@@ -64,26 +78,48 @@ struct GlobeSelectionView: View {
             }
         }
         .frame(height: Self.height)
-        .background(.regularMaterial, in: RoundedRectangle(cornerRadius: cornerRadius))
-        .hoverEffect()
+        .background(.regularMaterial.opacity(globeIsSelected ? 0 : 1), in: RoundedRectangle(cornerRadius: cornerRadius))
+        .hoverEffect(isEnabled: !globeIsSelected)
         .glassBackgroundEffect(in: RoundedRectangle(cornerRadius: cornerRadius))
-        .onTapGesture {
-            if model.selectedGlobeConfiguration == nil {
-                model.selectedGlobeConfiguration = GlobeEntity.Configuration(
-                    globe: globe,
-                    position: [globe.radius, globe.radius, -(globe.radius + 0.5)],
-                    speed: 0.1,
-                    isPaused: false,
-                    usePreviewTexture: false,
-                    enableGestures: true,
-                    addHoverEffect: false
-                )
-            } else {
-                Task {
-                    if let configuration = model.selectedGlobeConfiguration {
-                        configuration.globe = globe
-                        configuration.globeEntity = try await GlobeEntity(configuration: configuration)
-                    }
+        .onTapGesture(perform: loadGlobe)
+    }
+    
+    
+    /// True if the globe that can be selected by this view is already selected.
+    private var globeIsSelected: Bool {
+        model.selectedGlobeConfiguration?.globe.id == configuration.globe.id
+    }
+    
+    /// Async loading of globe.
+    private func loadGlobe() {
+        guard !globeIsSelected else { return }
+        
+        // create a new configuration if there is no selected globe
+        let r = configuration.globe.radius
+        if model.selectedGlobeConfiguration == nil {
+            model.selectedGlobeConfiguration = GlobeEntity.Configuration(
+                globe: configuration.globe,
+                position: [r, r, -(r + 0.5)],
+                speed: 0.1,
+                isPaused: false,
+                usePreviewTexture: false,
+                enableGestures: true,
+                addHoverEffect: false
+            )
+        }
+        
+        Task {
+            if let selectedGlobeConfiguration = model.selectedGlobeConfiguration {
+                withAnimation {
+                    loadingTexture = true
+                }
+                // replace the globe metadata, but reuse other settings, such as position, rotation, etc.
+                selectedGlobeConfiguration.globe = configuration.globe
+                
+                // load the globe
+                selectedGlobeConfiguration.globeEntity = try await GlobeEntity(configuration: selectedGlobeConfiguration)
+                withAnimation {
+                    loadingTexture = false
                 }
             }
         }
