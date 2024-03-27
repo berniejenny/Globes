4fc7e81aa6f029ff68300ab0d6f55b5375c6816a
berniejenny <berniejenny@users.noreply.github.com>
2024-03-27 18:30:53 +1100

    Improve gesture instructions.

--- Globes/Views/IntroView.swift
+++ Globes/Views/IntroView.swift
@@ -23,10 +23,10 @@ struct IntroView: View {
             
             Group {
                 Text("Choose a globe from the list.")
-                Text("To position the globe, pinch and drag it with one hand.")
-                Text("To resize the globe, pinch and drag it with both hands.")
-                Text("To rotate the globe, pinch it with one hand, hold for a moment and then drag, or pinch and rotate with both hands.")
-                Text("Double-pinch to stop and start the rotation.")
+                Text("To position the globe, pinch and drag with one hand.")
+                Text("To resize the globe, pinch and drag with both hands.")
+                Text("To rotate the globe, pinch with one hand, hold for a moment and then drag. Or, pinch and rotate with both hands.")
+                Text("Double-pinch to stop and start the automatic rotation.")
             }
             .font(.callout)
             .foregroundStyle(.secondary)
