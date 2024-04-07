cc7c67bf0eb65fe77a13cc1a030898a5dbdccf7d
berniejenny <berniejenny@users.noreply.github.com>
2024-04-08 09:17:38 +1000

    WebViewDecorated

--- Globes/Views/ContentView.swift
+++ Globes/Views/ContentView.swift
@@ -19,13 +19,11 @@ struct ContentView: View {
     @Environment(\.openImmersiveSpace) var openImmersiveSpace
     @Environment(\.dismissImmersiveSpace) var dismissImmersiveSpace
     
-    @State private var webViewStatus: WebViewStatus = .loading
-    
     var body: some View {
         
         Group {
             if model.webURL != nil {
-                webView
+                WebViewDecorated()
             } else {
                 navigationView
             }
@@ -55,45 +53,6 @@ struct ContentView: View {
             })
     }
     
-    @ViewBuilder private var webView: some View {
-        let url = model.webURL ?? URL(string: "https://www.davidrumsey.com")!
-        
-        ZStack {
-            WebView(url: url, status: $webViewStatus)
-            
-            switch webViewStatus {
-            case .loading:
-                ProgressView("Loading Page")
-                    .padding()
-                    .glassBackgroundEffect(in: RoundedRectangle(cornerRadius: 20))
-            case .finishedLoading:
-                EmptyView()
-            case .failed(let error):
-                VStack {
-                    Text("The page could not be loaded.")
-                    Text(error.localizedDescription)
-                }
-                .padding()
-                .glassBackgroundEffect(in: RoundedRectangle(cornerRadius: 20))
-            }
-        }
-        .ornament(attachmentAnchor: .scene(.bottom)) {
-            HStack {
-                Button(action: {
-                    model.webURL = nil
-                }) {
-                    Label("Go Back to Globes", systemImage: "chevron.left")
-                }
-                .labelStyle(.iconOnly)
-                .help("Go Back to Globes")
-                .padding()
-                
-                Link("Open in Safari", destination: url)
-                    .padding()
-            }
-            .glassBackgroundEffect()
-        }
-    }
     
     @ViewBuilder private var navigationView: some View {
         NavigationSplitView {
